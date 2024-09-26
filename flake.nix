5e81ae77f36636c458c4b89d932a63c113e52e02
Abel Svoboda <abel465@gmail.com>
2024-09-26 12:09:45 +1200

    Use flake-parts instead of flake-utils

--- flake.nix
+++ flake.nix
@@ -6,78 +6,82 @@
       url = "github:nix-community/fenix/5708f08c8bcb6dd98b573a162e05cd5aa506091e";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
-    flake-utils,
+    flake-parts,
     fenix,
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [fenix.overlays.default];
-      pkgs = import nixpkgs {inherit overlays system;};
-
-      rustPkg = fenix.packages.${system}.latest.withComponents [
-        "rust-src"
-        "rustc-dev"
-        "llvm-tools-preview"
-        "cargo"
-        "clippy"
-        "rustc"
-        "rustfmt"
-        "rust-analyzer"
-      ];
-      rustPlatform = pkgs.makeRustPlatform {
-        cargo = rustPkg;
-        rustc = rustPkg;
-      };
-      buildInputs = with pkgs; [
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-        vulkan-loader
-        vulkan-tools
-        wayland
-        libxkbcommon
-      ];
-      shadersCompilePath = "$HOME/.cache/rust-gpu-shaders";
-    in rec {
-      rustGpuShaders = rustPlatform.buildRustPackage {
-        pname = "rust-gpu-shaders";
-        version = "0.0.0";
-        src = ./.;
-        cargoHash = "";
-        cargoLock.lockFile = ./Cargo.lock;
-        cargoLock.outputHashes = {
-          "rustc_codegen_spirv-0.9.0" = "sha256-6QENP2ttWrtykfv+TUfjGrOajkN2X9cHYINauFZiup8=";
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        rustPkg = fenix.packages.${system}.latest.withComponents [
+          "rust-src"
+          "rustc-dev"
+          "llvm-tools-preview"
+          "cargo"
+          "clippy"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+        ];
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustPkg;
+          rustc = rustPkg;
         };
-        nativeBuildInputs = [pkgs.makeWrapper];
-        configurePhase = ''
-          export SHADERS_DIR="$out/repo/shaders"
-          export SHADERS_TARGET_DIR=${shadersCompilePath}
-        '';
-        fixupPhase = ''
-          cp -r . $out/repo
-          wrapProgram $out/bin/runner \
-            --set LD_LIBRARY_PATH $LD_LIBRARY_PATH:$out/lib:${nixpkgs.lib.makeLibraryPath buildInputs} \
-            --set PATH $PATH:${nixpkgs.lib.makeBinPath [rustPkg]}
-        '';
-      };
-      packages.default = pkgs.writeShellScriptBin "rust-gpu-shaders" ''
-        export CARGO_TARGET_DIR="${shadersCompilePath}"
-        exec -a "$0" "${rustGpuShaders}/bin/runner" "$@"
-      '';
-      apps.default = {
-        type = "app";
-        program = "${packages.default}/bin/rust-gpu-shaders";
-      };
-      devShell = with pkgs;
-        mkShell {
-          nativeBuildInputs = [rustPkg];
-          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        buildInputs = with pkgs; [
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXi
+          vulkan-loader
+          vulkan-tools
+          wayland
+          libxkbcommon
+        ];
+        shadersCompilePath = "$HOME/.cache/rust-gpu-shaders";
+        rustGpuShaders = rustPlatform.buildRustPackage {
+          pname = "rust-gpu-shaders";
+          version = "0.0.0";
+          src = ./.;
+          cargoHash = "";
+          cargoLock.lockFile = ./Cargo.lock;
+          cargoLock.outputHashes = {
+            "rustc_codegen_spirv-0.9.0" = "sha256-6QENP2ttWrtykfv+TUfjGrOajkN2X9cHYINauFZiup8=";
+          };
+          nativeBuildInputs = [pkgs.makeWrapper];
+          configurePhase = ''
+            export SHADERS_DIR="$out/repo/shaders"
+            export SHADERS_TARGET_DIR=${shadersCompilePath}
+          '';
+          fixupPhase = ''
+            cp -r . $out/repo
+            wrapProgram $out/bin/runner \
+              --set LD_LIBRARY_PATH $LD_LIBRARY_PATH:$out/lib:${nixpkgs.lib.makeLibraryPath buildInputs} \
+              --set PATH $PATH:${nixpkgs.lib.makeBinPath [rustPkg]}
+          '';
         };
-    });
+      in rec {
+        packages.default = pkgs.writeShellScriptBin "rust-gpu-shaders" ''
+          export CARGO_TARGET_DIR="${shadersCompilePath}"
+          exec -a "$0" "${rustGpuShaders}/bin/runner" "$@"
+        '';
+        apps.default = {
+          type = "app";
+          program = "${packages.default}/bin/rust-gpu-shaders";
+        };
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [rustPkg];
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          };
+      };
+    };
 }
